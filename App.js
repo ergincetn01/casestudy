5a1113e60e8ab32bc86fb35c30d8ebfa92881a69
ergincetn01 <ergincetn01@gmail.com>
2023-09-13 00:15:04 +0300

    basic prj. structure created

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+
+
+const Stack= createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen 
+      // name="Home" 
+      // component={}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 24,
+    padding: 16,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
